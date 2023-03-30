07e516db8180ddef821178b342bdab974ba5bb49
Syed Manan Hussain Andrabi <syedmanan.huss@gmail.com>
2023-03-30 11:25:27 +0530

    Initial commit

---
+++ ClassBased/Car.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Metadata;
+namespace ClassBased
+{
+
+   class Car
+    {
+        public Car(string modelName, string modelColor, int modelYear)
+        {
+            model = modelName;
+            color = modelColor;
+            year = modelYear;
+        }
+
+
+        public string model;
+        public string color;
+        public int year;
+        public static void fullThrottle()
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+    }
+}
---
+++ ClassBased/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClassBased;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Car Ford = new Car("Mustang", "Red", 1969);
+        Car Kia = new Car("Carrens", "White", 2020);
+
+
+        Console.WriteLine(Ford.color + " " + Ford.year + " " + Ford.model);
+        Console.WriteLine(Kia.color + " " + Kia.year + " " + Kia.model);
+
+       
+        Car.fullThrottle();
+
+
+        
+    }
+}
